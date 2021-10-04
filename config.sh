205d1486798fc8133bb692ad4265cb51bb5d4475
Joshua Johanning <jjohanning@10thmagnitude.com>
2021-10-04 14:33:14 -0500

    adding config script to set environment variables

---
+++ config.sh
@@ -0,0 +1,13 @@
+# npm install 
+# npm install -g typescript
+# tsc Code/main.ts
+# node Code/main.js
+
+export AZURE_USERNAME="sa-azuredevopspat@myaad.onmicrosoft.com"
+export AZURE_PASSWORD="password"
+
+export AZURE_CLIENT_ID="b26e4a7b-xxxx-xxx-xxxx-b5bb521bfa2b"
+export AZURE_TENANT_ID="e9846558-xxxx-xxxx-xxxx-ebebe80779a1"
+export AZURE_CLIENT_SECRET="xxxx"
+
+export AZURE_DEVOPS_ORG="soccerjoshj07"
